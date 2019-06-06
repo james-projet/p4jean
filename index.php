94d8c8a93cf0a507987741485af2b33a1d613b89
james <james.gaffe@yahoo.fr>
2019-06-06 13:15:54 +0200

    page index

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-page principal du projet
+<?php
+include('_config.php');
+
+MyAutoload::start();
+// url :    index.php?action=homepage    - index.php?action=contact     index.php
+
+(isset($_GET['action'])) ? $action = $_GET['action'] : $action = "homepage";
+(isset($_GET['id'])) ? $id = $_GET['id'] : $id = "?";
+
+// role de router
+$routeur = new Routeur($action);
+$routeur->renderController();
