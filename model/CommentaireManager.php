d6327c6b4ef19a761ad5dcac5f786714bc4f12c8
james <james.gaffe@yahoo.fr>
2019-06-06 14:54:58 +0200

    classe manager commentaire

---
+++ model/CommentaireManager.php
@@ -0,0 +1,50 @@
+<?php
+class EpisodeManager
+{
+  private $bdd;
+
+  public function __construct()
+  {
+    $this->bdd = new PDO('mysql:host=localhost;dbname=p4jean;charset=utf8', 'root', '');
+  }
+
+  /**
+  *Sert a renvoyer un commentaire par rapport a son id
+  *$id = int
+  *return l objet episode = objet
+  **/
+  public function findCommentaireById($id)
+  {
+    $commentaires = array();
+    $bdd = $this->bdd;
+    $reponse = $bdd->prepare('SELECT pseudo, commentaire, date_creation, episode_id FROM commentaire WHERE episode_id=:id');
+    $reponse->bindValue('id', $id);
+    $reponse->execute();
+    while ($donnees = $reponse->fetch())
+    {
+     $commentaire = new Commentaire();
+     $commentaire->setPseudo($donnees['pseudo']);
+     $commentaire->setCommentaire($donnees['commentaire']);
+     $commentaire->setDate_creation($donnees['date_creation']);
+     $commentaire->setEpisode_id($donnees['episode_id']);
+
+     $commentaires[] = $commentaire;
+   };
+
+      return $commentaires;
+
+  }
+  /**
+  *Sert a stocker les commentaire en BDD
+  *$pseudo= string , $commentaire = string, $episode_id = int
+  **/
+  public function stockageCommentaire($pseudo, $commentaire, $episode_id)
+  {
+    $bdd = $this->bdd;
+
+    $req = $bdd->prepare('INSERT INTO commentaire (pseudo, commentaire, episode_id, date_creation) VALUES(?, ?, ?, NOW())');
+    $req->execute(array($pseudo, $commentaire, $episode_id));
+  }
+}
+
+}
