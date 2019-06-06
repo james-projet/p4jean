6f956ecd069ee6bf436620406c6cc9c408bea727
james <james.gaffe@yahoo.fr>
2019-06-06 15:02:04 +0200

    vue contact

---
+++ view/contact.php
@@ -0,0 +1,8 @@
+<form id="formcontact" action="contact" method="post">
+  <p>
+  Nom : <input type="text" name="nom" id="nom" /><br />
+  Prenom : <input type="text" name="prenom" id="prenom" /><br />
+  <textarea name="name" rows="8" cols="40">Message</textarea>
+  <input type="submit" value="Envoyer">
+  </p>
+</form>
